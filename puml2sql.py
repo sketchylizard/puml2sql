3ca3e85c24b76dde26850106025f06fc804c71f0
Jason Stewart <jason.stewart@garmin.com>
2021-08-31 10:04:51 -0400

    Cleaned up global state
    Change-Id: I6ec0485dc51b7dc001389650b42a4d4d0235308e

--- puml2sql.py
+++ puml2sql.py
@@ -12,26 +12,7 @@ parser.add_argument("file")
 # Parse the command line.
 args = parser.parse_args()
 
-current = None
 tables = []
-
-class Table:
-    def __init__(self, name):
-        self.name = name
-        self.fields = []
-        self.constraints = []
-        self.primaries = []
-
-def lookingForTable(line):
-    global current
-    global state
-    assert(current == None)
-    if line[:6] == "table(":
-        line = line[6:]
-        line = line[:line.find(')')]
-        #print('Found table', line)
-        current = Table(line)
-        state = parse_column
 
 class column:
     def __init__(self, name, type):
@@ -43,60 +24,75 @@ class column:
         self.unique = False
 
 class unique:
-    def __init__(self, table, line):
+    def __init__(self, line):
+        assert(line.startswith('unique('))
+        assert(line[-1] == ')')
+        line = line[7:-1]
         self.name = 'unique'
         self.fields = map(lambda p : p.strip(), line.split(','))
-        table.constraints.append(self)
-        #print('    unique: {}'.format(','.join(self.fields)))
 
-def parse_TableConstraint(line):
-    global current
-    global state
-    assert(current != None)
-    assert(line != '')
-    if line.startswith('unique('):
-        pass
+class Table:
+    def __init__(self, name):
+        self.name = name
+        self.fields = []
+        self.constraints = []
+        self.primaries = []
+
+    def parse_TableConstraint(self, line):
+        assert(line != '')
+        if line.startswith('unique('):
+            c = unique(line)
+            self.constraints.append(c)
+            return self.parse_TableConstraint
+        else:
+            # end of table
+            assert(line == '}')
+            return lookingForTable
+
+    def parse_column(self, line):
+        assert(line != '')
+        # get the name & type
+        m = re.match('(\w+)(\s+|\s*:\s*)(\w+)\s*', line)
+        if m:
+            name = m[1]
+            type = m[3]
+            line = line[m.end(0):]
+
+            f = column(name, type)
+
+            while line != '':
+                m  = re.match('(primary|notnull|unique|generated)\s*', line)
+                if m[1] == 'primary':
+                    f.primary = True
+                    line = line[m.end(0):]
+                    self.primaries.append(name)
+                elif m[1] == 'notnull':
+                    f.notNull = True
+                    line = line[m.end(0):]
+                elif m[1] == 'unique':
+                    f.unique = True
+                elif m[1] == 'generated':
+                    f.generated = line[m.end(0):]
+                    line = ''
+                else:
+                    raise Exception("Unknown token {}".format(line))
+            self.fields.append(f)
+            return self.parse_column
+        else:
+            # we must have hit the end of the columns
+            # look for table constraints now
+            return self.parse_TableConstraint(line)
+
+def lookingForTable(line):
+    if line[:6] == "table(":
+        line = line[6:]
+        line = line[:line.find(')')]
+        #print('Found table', line)
+        table = Table(line)
+        tables.append(table)
+        return table.parse_column
     else:
-        # end of table
-        assert(line == '}')
-        tables.append(current)
-        current = None
-        state = lookingForTable
-
-def parse_column(line):
-    global current
-    global state
-    assert(current != None)
-    assert(line != '')
-    # get the name & type
-    m = re.match('(\w+)(\s+|\s*:\s*)(\w+)\s*', line)
-    if m:
-        name = m[1]
-        type = m[3]
-        line = line[m.end(0):]
-
-        f = column(name, type)
-
-        while line != '':
-            m  = re.match('(primary|notnull|generated)\s*', line)
-            if m[1] == 'primary':
-                f.primary = True
-                line = line[m.end(0):]
-                current.primaries.append(name)
-            elif m[1] == 'notnull':
-                f.notNull = True
-                line = line[m.end(0):]
-            elif m[1] == 'generated':
-                f.generated = line[m.end(0):]
-                line = ''
-            else:
-                raise Exception("Unknown token {}".format(line))
-        current.fields.append(f)
-    else:
-        # we must have hit the end of the columns
-        # look for table constraints now
-        state = parse_TableConstraint
-        return parse_TableConstraint(line)
+        return lookingForTable
 
 state = lookingForTable
 
@@ -104,13 +100,17 @@ def cleanup(s):
     p = s.find("'")
     if p >= 0:
         s = s[:p]
-    return s.strip()
+    # skip comments
+    s = s.strip()
+    if s == '---' or l == '':
+        s = None
+    return s
 
 with open(args.file) as f:
     for l in f:
         l = cleanup(l)
-        if l != '---' and l != '':
-            state(l)
+        if l:
+            state = state(l)
 
 class SqlFormatter:
     def __init__(self):
@@ -119,13 +119,11 @@ class SqlFormatter:
     def column(self, table, f):
 		# If only one field is marked as a primary key
 		# then we should add that as a field constraint.
-        primary = ' PRIMARY KEY' if (len(table.primaries) == 1 and f.primary) else ''
-        notnull = ' NOT NULL' if f.notNull else ''
-        generated = ' AS {}'.format(f.generated) if f.generated else ''
-        return '	{} {}{}{}{}'.format(f.name, f.type.upper(), primary, notnull, generated)
-
-    def generated(self, table, f):
-        return '	{} {} AS {}'.format(f.name, f.type.upper(), f.expression)
+        constraint =  ' PRIMARY KEY' if (len(table.primaries) == 1 and f.primary) else ''
+        constraint += ' NOT NULL' if f.notNull else ''
+        constraint += ' UNIQUE' if f.unique else ''
+        constraint += ' AS {}'.format(f.generated) if f.generated else ''
+        return '	{} {}{}'.format(f.name, f.type.upper(), constraint)
 
     def constraint(self, table, name, fields):
         return '	{}({})'.format(name.upper(), ', '.join(fields))
